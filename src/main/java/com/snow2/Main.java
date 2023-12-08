f03f6a6eed0820c960da5d2c9810d553d2e43ba2
Azukirice <kzksx_180@yahoo.co.jp>
2023-12-09 02:18:31 +0900

    コード整形

--- src/main/java/com/snow2/Main.java
+++ src/main/java/com/snow2/Main.java
@@ -16,9 +16,10 @@ public class Main {
         skiResort.put("芸北国際スキー場", skiSlope3);
         skiResort.put("恐羅漢スキー場", skiSlope4);
 
+//      入力したスキー場の詳細を表示する
         System.out.println("ハチ高原 瑞穂ハイランド 芸北国際スキー場 恐羅漢スキー場");
         System.out.println("スキー場の名前を入力してください");
-//      入力したスキー場の詳細を表示する
+
         String resortName = new java.util.Scanner(System.in).nextLine();
         for (String key : skiResort.keySet()) {
             if (key.equals(resortName)) {
@@ -28,6 +29,8 @@ public class Main {
                 System.out.println("");
             }
         }
+
+//      利用人数をintで入力する例外処理
         System.out.println("ゲストハウスの利用人数を入力してください");
         try {
             int guest = new java.util.Scanner(System.in).nextInt();
